82a531c6e3d3658c8664d511f4140ab5026ab0b4
TaoWang <48122403+bigtaoo@users.noreply.github.com>
2024-09-09 20:18:10 +0200

    tools.

--- Unity/Assets/Scripts/Model/Generate/Server/Message/MicroDustClientMessage_C_30000.cs
+++ Unity/Assets/Scripts/Model/Generate/Server/Message/MicroDustClientMessage_C_30000.cs
@@ -1,84 +1,90 @@
-using ET;
 using MemoryPack;
 using System.Collections.Generic;
+
 namespace ET
 {
-	[ResponseType(nameof(NetClient2Main_MicroDust_Login))]
-	[Message(MicroDustClientMessage.Main2NetClient_MicroDust_Login)]
-	[MemoryPackable]
-	public partial class Main2NetClient_MicroDust_Login: MessageObject, IRequest
-	{
-		public static Main2NetClient_MicroDust_Login Create(bool isFromPool = true) 
-		{ 
-			return !isFromPool? new Main2NetClient_MicroDust_Login() : ObjectPool.Instance.Fetch(typeof(Main2NetClient_MicroDust_Login)) as Main2NetClient_MicroDust_Login; 
-		}
+    [MemoryPackable]
+    [Message(MicroDustClientMessage.Main2NetClient_MicroDust_Login)]
+    [ResponseType(nameof(NetClient2Main_MicroDust_Login))]
+    public partial class Main2NetClient_MicroDust_Login : MessageObject, IRequest
+    {
+        public static Main2NetClient_MicroDust_Login Create(bool isFromPool = false)
+        {
+            return ObjectPool.Instance.Fetch(typeof(Main2NetClient_MicroDust_Login), isFromPool) as Main2NetClient_MicroDust_Login;
+        }
 
-		[MemoryPackOrder(0)]
-		public int RpcId { get; set; }
+        [MemoryPackOrder(0)]
+        public int RpcId { get; set; }
 
-		[MemoryPackOrder(1)]
-		public int OwnerFiberId { get; set; }
+        [MemoryPackOrder(1)]
+        public int OwnerFiberId { get; set; }
 
-		[MemoryPackOrder(2)]
-		public string Account { get; set; }
+        [MemoryPackOrder(2)]
+        public string Account { get; set; }
 
-		[MemoryPackOrder(3)]
-		public string Password { get; set; }
+        [MemoryPackOrder(3)]
+        public string Password { get; set; }
 
-		public override void Dispose() 
-		{
-			if (!this.IsFromPool) return;
-			this.RpcId = default;
-			this.OwnerFiberId = default;
-			this.Account = default;
-			this.Password = default;
-			
-			ObjectPool.Instance.Recycle(this); 
-		}
+        public override void Dispose()
+        {
+            if (!this.IsFromPool)
+            {
+                return;
+            }
 
-	}
+            this.RpcId = default;
+            this.OwnerFiberId = default;
+            this.Account = default;
+            this.Password = default;
 
-	[Message(MicroDustClientMessage.NetClient2Main_MicroDust_Login)]
-	[MemoryPackable]
-	public partial class NetClient2Main_MicroDust_Login: MessageObject, IResponse
-	{
-		public static NetClient2Main_MicroDust_Login Create(bool isFromPool = true) 
-		{ 
-			return !isFromPool? new NetClient2Main_MicroDust_Login() : ObjectPool.Instance.Fetch(typeof(NetClient2Main_MicroDust_Login)) as NetClient2Main_MicroDust_Login; 
-		}
+            ObjectPool.Instance.Recycle(this);
+        }
+    }
 
-		[MemoryPackOrder(0)]
-		public int RpcId { get; set; }
+    [MemoryPackable]
+    [Message(MicroDustClientMessage.NetClient2Main_MicroDust_Login)]
+    public partial class NetClient2Main_MicroDust_Login : MessageObject, IResponse
+    {
+        public static NetClient2Main_MicroDust_Login Create(bool isFromPool = false)
+        {
+            return ObjectPool.Instance.Fetch(typeof(NetClient2Main_MicroDust_Login), isFromPool) as NetClient2Main_MicroDust_Login;
+        }
 
-		[MemoryPackOrder(1)]
-		public int Error { get; set; }
+        [MemoryPackOrder(0)]
+        public int RpcId { get; set; }
 
-		[MemoryPackOrder(2)]
-		public string Message { get; set; }
+        [MemoryPackOrder(1)]
+        public int Error { get; set; }
 
-		[MemoryPackOrder(3)]
-		public long PlayerId { get; set; }
+        [MemoryPackOrder(2)]
+        public string Message { get; set; }
 
-		[MemoryPackOrder(4)]
-		public string UserId { get; set; }
+        [MemoryPackOrder(3)]
+        public long PlayerId { get; set; }
 
-		public override void Dispose() 
-		{
-			if (!this.IsFromPool) return;
-			this.RpcId = default;
-			this.Error = default;
-			this.Message = default;
-			this.PlayerId = default;
-			this.UserId = default;
-			
-			ObjectPool.Instance.Recycle(this); 
-		}
+        [MemoryPackOrder(4)]
+        public string UserId { get; set; }
 
-	}
+        public override void Dispose()
+        {
+            if (!this.IsFromPool)
+            {
+                return;
+            }
 
-	public static class MicroDustClientMessage
-	{
-		 public const ushort Main2NetClient_MicroDust_Login = 30001;
-		 public const ushort NetClient2Main_MicroDust_Login = 30002;
-	}
+            this.RpcId = default;
+            this.Error = default;
+            this.Message = default;
+            this.PlayerId = default;
+            this.UserId = default;
+
+            ObjectPool.Instance.Recycle(this);
+        }
+    }
+
+    public static class MicroDustClientMessage
+    {
+        public const ushort Main2NetClient_MicroDust_Login = 30001;
+        public const ushort NetClient2Main_MicroDust_Login = 30002;
+    }
 }
--- Unity/Assets/Scripts/Model/Generate/Server/Message/MicroDustInner_S_31001.cs
+++ Unity/Assets/Scripts/Model/Generate/Server/Message/MicroDustInner_S_31001.cs
@@ -1,229 +1,250 @@
-using ET;
 using MemoryPack;
 using System.Collections.Generic;
+
 namespace ET
 {
-	[ResponseType(nameof(G2G_MicroDust_LockResponse))]
-	[Message(MicroDustInner.G2G_MicroDust_LockRequest)]
-	[MemoryPackable]
-	public partial class G2G_MicroDust_LockRequest: MessageObject, IRequest
-	{
-		public static G2G_MicroDust_LockRequest Create(bool isFromPool = true) 
-		{ 
-			return !isFromPool? new G2G_MicroDust_LockRequest() : ObjectPool.Instance.Fetch(typeof(G2G_MicroDust_LockRequest)) as G2G_MicroDust_LockRequest; 
-		}
+    [MemoryPackable]
+    [Message(MicroDustInner.G2G_MicroDust_LockRequest)]
+    [ResponseType(nameof(G2G_MicroDust_LockResponse))]
+    public partial class G2G_MicroDust_LockRequest : MessageObject, IRequest
+    {
+        public static G2G_MicroDust_LockRequest Create(bool isFromPool = false)
+        {
+            return ObjectPool.Instance.Fetch(typeof(G2G_MicroDust_LockRequest), isFromPool) as G2G_MicroDust_LockRequest;
+        }
 
-		[MemoryPackOrder(0)]
-		public int RpcId { get; set; }
+        [MemoryPackOrder(0)]
+        public int RpcId { get; set; }
 
-		[MemoryPackOrder(1)]
-		public long Id { get; set; }
+        [MemoryPackOrder(1)]
+        public long Id { get; set; }
 
-		[MemoryPackOrder(2)]
-		public string Address { get; set; }
+        [MemoryPackOrder(2)]
+        public string Address { get; set; }
 
-		public override void Dispose() 
-		{
-			if (!this.IsFromPool) return;
-			this.RpcId = default;
-			this.Id = default;
-			this.Address = default;
-			
-			ObjectPool.Instance.Recycle(this); 
-		}
+        public override void Dispose()
+        {
+            if (!this.IsFromPool)
+            {
+                return;
+            }
 
-	}
+            this.RpcId = default;
+            this.Id = default;
+            this.Address = default;
 
-	[Message(MicroDustInner.G2G_MicroDust_LockResponse)]
-	[MemoryPackable]
-	public partial class G2G_MicroDust_LockResponse: MessageObject, IResponse
-	{
-		public static G2G_MicroDust_LockResponse Create(bool isFromPool = true) 
-		{ 
-			return !isFromPool? new G2G_MicroDust_LockResponse() : ObjectPool.Instance.Fetch(typeof(G2G_MicroDust_LockResponse)) as G2G_MicroDust_LockResponse; 
-		}
+            ObjectPool.Instance.Recycle(this);
+        }
+    }
 
-		[MemoryPackOrder(0)]
-		public int RpcId { get; set; }
+    [MemoryPackable]
+    [Message(MicroDustInner.G2G_MicroDust_LockResponse)]
+    public partial class G2G_MicroDust_LockResponse : MessageObject, IResponse
+    {
+        public static G2G_MicroDust_LockResponse Create(bool isFromPool = false)
+        {
+            return ObjectPool.Instance.Fetch(typeof(G2G_MicroDust_LockResponse), isFromPool) as G2G_MicroDust_LockResponse;
+        }
 
-		[MemoryPackOrder(1)]
-		public int Error { get; set; }
+        [MemoryPackOrder(0)]
+        public int RpcId { get; set; }
 
-		[MemoryPackOrder(2)]
-		public string Message { get; set; }
+        [MemoryPackOrder(1)]
+        public int Error { get; set; }
 
-		public override void Dispose() 
-		{
-			if (!this.IsFromPool) return;
-			this.RpcId = default;
-			this.Error = default;
-			this.Message = default;
-			
-			ObjectPool.Instance.Recycle(this); 
-		}
+        [MemoryPackOrder(2)]
+        public string Message { get; set; }
 
-	}
+        public override void Dispose()
+        {
+            if (!this.IsFromPool)
+            {
+                return;
+            }
 
-	[Message(MicroDustInner.G2M_MicroDust_SessionDisconnect)]
-	[MemoryPackable]
-	public partial class G2M_MicroDust_SessionDisconnect: MessageObject, ILocationMessage
-	{
-		public static G2M_MicroDust_SessionDisconnect Create(bool isFromPool = true) 
-		{ 
-			return !isFromPool? new G2M_MicroDust_SessionDisconnect() : ObjectPool.Instance.Fetch(typeof(G2M_MicroDust_SessionDisconnect)) as G2M_MicroDust_SessionDisconnect; 
-		}
+            this.RpcId = default;
+            this.Error = default;
+            this.Message = default;
 
-		[MemoryPackOrder(0)]
-		public int RpcId { get; set; }
+            ObjectPool.Instance.Recycle(this);
+        }
+    }
 
-		public override void Dispose() 
-		{
-			if (!this.IsFromPool) return;
-			this.RpcId = default;
-			
-			ObjectPool.Instance.Recycle(this); 
-		}
+    [MemoryPackable]
+    [Message(MicroDustInner.G2M_MicroDust_SessionDisconnect)]
+    public partial class G2M_MicroDust_SessionDisconnect : MessageObject, ILocationMessage
+    {
+        public static G2M_MicroDust_SessionDisconnect Create(bool isFromPool = false)
+        {
+            return ObjectPool.Instance.Fetch(typeof(G2M_MicroDust_SessionDisconnect), isFromPool) as G2M_MicroDust_SessionDisconnect;
+        }
 
-	}
+        [MemoryPackOrder(0)]
+        public int RpcId { get; set; }
 
-	[ResponseType(nameof(M2M_MicroDust_UnitTransferResponse))]
-	[Message(MicroDustInner.M2M_MicroDust_UnitTransferRequest)]
-	[MemoryPackable]
-	public partial class M2M_MicroDust_UnitTransferRequest: MessageObject, IRequest
-	{
-		public static M2M_MicroDust_UnitTransferRequest Create(bool isFromPool = true) 
-		{ 
-			return !isFromPool? new M2M_MicroDust_UnitTransferRequest() : ObjectPool.Instance.Fetch(typeof(M2M_MicroDust_UnitTransferRequest)) as M2M_MicroDust_UnitTransferRequest; 
-		}
+        public override void Dispose()
+        {
+            if (!this.IsFromPool)
+            {
+                return;
+            }
 
-		[MemoryPackOrder(0)]
-		public int RpcId { get; set; }
+            this.RpcId = default;
 
-		[MemoryPackOrder(1)]
-		public ActorId OldActorId { get; set; }
+            ObjectPool.Instance.Recycle(this);
+        }
+    }
 
-		[MemoryPackOrder(2)]
-		public byte[] Unit { get; set; }
+    [MemoryPackable]
+    [Message(MicroDustInner.M2M_MicroDust_UnitTransferRequest)]
+    [ResponseType(nameof(M2M_MicroDust_UnitTransferResponse))]
+    public partial class M2M_MicroDust_UnitTransferRequest : MessageObject, IRequest
+    {
+        public static M2M_MicroDust_UnitTransferRequest Create(bool isFromPool = false)
+        {
+            return ObjectPool.Instance.Fetch(typeof(M2M_MicroDust_UnitTransferRequest), isFromPool) as M2M_MicroDust_UnitTransferRequest;
+        }
 
-		[MemoryPackOrder(3)]
-		public List<byte[]> Entitys { get; set; } = new();
+        [MemoryPackOrder(0)]
+        public int RpcId { get; set; }
 
-		public override void Dispose() 
-		{
-			if (!this.IsFromPool) return;
-			this.RpcId = default;
-			this.OldActorId = default;
-			this.Unit = default;
-			this.Entitys.Clear();
-			
-			ObjectPool.Instance.Recycle(this); 
-		}
+        [MemoryPackOrder(1)]
+        public ActorId OldActorId { get; set; }
 
-	}
+        [MemoryPackOrder(2)]
+        public byte[] Unit { get; set; }
 
-	[Message(MicroDustInner.M2M_MicroDust_UnitTransferResponse)]
-	[MemoryPackable]
-	public partial class M2M_MicroDust_UnitTransferResponse: MessageObject, IResponse
-	{
-		public static M2M_MicroDust_UnitTransferResponse Create(bool isFromPool = true) 
-		{ 
-			return !isFromPool? new M2M_MicroDust_UnitTransferResponse() : ObjectPool.Instance.Fetch(typeof(M2M_MicroDust_UnitTransferResponse)) as M2M_MicroDust_UnitTransferResponse; 
-		}
+        [MemoryPackOrder(3)]
+        public List<byte[]> Entitys { get; set; } = new();
 
-		[MemoryPackOrder(0)]
-		public int RpcId { get; set; }
+        public override void Dispose()
+        {
+            if (!this.IsFromPool)
+            {
+                return;
+            }
 
-		[MemoryPackOrder(1)]
-		public int Error { get; set; }
+            this.RpcId = default;
+            this.OldActorId = default;
+            this.Unit = default;
+            this.Entitys.Clear();
 
-		[MemoryPackOrder(2)]
-		public string Message { get; set; }
+            ObjectPool.Instance.Recycle(this);
+        }
+    }
 
-		public override void Dispose() 
-		{
-			if (!this.IsFromPool) return;
-			this.RpcId = default;
-			this.Error = default;
-			this.Message = default;
-			
-			ObjectPool.Instance.Recycle(this); 
-		}
+    [MemoryPackable]
+    [Message(MicroDustInner.M2M_MicroDust_UnitTransferResponse)]
+    public partial class M2M_MicroDust_UnitTransferResponse : MessageObject, IResponse
+    {
+        public static M2M_MicroDust_UnitTransferResponse Create(bool isFromPool = false)
+        {
+            return ObjectPool.Instance.Fetch(typeof(M2M_MicroDust_UnitTransferResponse), isFromPool) as M2M_MicroDust_UnitTransferResponse;
+        }
 
-	}
+        [MemoryPackOrder(0)]
+        public int RpcId { get; set; }
 
-	[ResponseType(nameof(G2R_MicroDust_LoginKey))]
-	[Message(MicroDustInner.R2G_MicroDust_LoginKey)]
-	[MemoryPackable]
-	public partial class R2G_MicroDust_LoginKey: MessageObject, IRequest
-	{
-		public static R2G_MicroDust_LoginKey Create(bool isFromPool = true) 
-		{ 
-			return !isFromPool? new R2G_MicroDust_LoginKey() : ObjectPool.Instance.Fetch(typeof(R2G_MicroDust_LoginKey)) as R2G_MicroDust_LoginKey; 
-		}
+        [MemoryPackOrder(1)]
+        public int Error { get; set; }
 
-		[MemoryPackOrder(0)]
-		public int RpcId { get; set; }
+        [MemoryPackOrder(2)]
+        public string Message { get; set; }
 
-		[MemoryPackOrder(1)]
-		public string Account { get; set; }
+        public override void Dispose()
+        {
+            if (!this.IsFromPool)
+            {
+                return;
+            }
 
-		public override void Dispose() 
-		{
-			if (!this.IsFromPool) return;
-			this.RpcId = default;
-			this.Account = default;
-			
-			ObjectPool.Instance.Recycle(this); 
-		}
+            this.RpcId = default;
+            this.Error = default;
+            this.Message = default;
 
-	}
+            ObjectPool.Instance.Recycle(this);
+        }
+    }
 
-	[Message(MicroDustInner.G2R_MicroDust_LoginKey)]
-	[MemoryPackable]
-	public partial class G2R_MicroDust_LoginKey: MessageObject, IResponse
-	{
-		public static G2R_MicroDust_LoginKey Create(bool isFromPool = true) 
-		{ 
-			return !isFromPool? new G2R_MicroDust_LoginKey() : ObjectPool.Instance.Fetch(typeof(G2R_MicroDust_LoginKey)) as G2R_MicroDust_LoginKey; 
-		}
+    [MemoryPackable]
+    [Message(MicroDustInner.R2G_MicroDust_LoginKey)]
+    [ResponseType(nameof(G2R_MicroDust_LoginKey))]
+    public partial class R2G_MicroDust_LoginKey : MessageObject, IRequest
+    {
+        public static R2G_MicroDust_LoginKey Create(bool isFromPool = false)
+        {
+            return ObjectPool.Instance.Fetch(typeof(R2G_MicroDust_LoginKey), isFromPool) as R2G_MicroDust_LoginKey;
+        }
 
-		[MemoryPackOrder(0)]
-		public int RpcId { get; set; }
+        [MemoryPackOrder(0)]
+        public int RpcId { get; set; }
 
-		[MemoryPackOrder(1)]
-		public int Error { get; set; }
+        [MemoryPackOrder(1)]
+        public string Account { get; set; }
 
-		[MemoryPackOrder(2)]
-		public string Message { get; set; }
+        public override void Dispose()
+        {
+            if (!this.IsFromPool)
+            {
+                return;
+            }
 
-		[MemoryPackOrder(3)]
-		public long Key { get; set; }
+            this.RpcId = default;
+            this.Account = default;
 
-		[MemoryPackOrder(4)]
-		public long GateId { get; set; }
+            ObjectPool.Instance.Recycle(this);
+        }
+    }
 
-		public override void Dispose() 
-		{
-			if (!this.IsFromPool) return;
-			this.RpcId = default;
-			this.Error = default;
-			this.Message = default;
-			this.Key = default;
-			this.GateId = default;
-			
-			ObjectPool.Instance.Recycle(this); 
-		}
+    [MemoryPackable]
+    [Message(MicroDustInner.G2R_MicroDust_LoginKey)]
+    public partial class G2R_MicroDust_LoginKey : MessageObject, IResponse
+    {
+        public static G2R_MicroDust_LoginKey Create(bool isFromPool = false)
+        {
+            return ObjectPool.Instance.Fetch(typeof(G2R_MicroDust_LoginKey), isFromPool) as G2R_MicroDust_LoginKey;
+        }
 
-	}
+        [MemoryPackOrder(0)]
+        public int RpcId { get; set; }
 
-	public static class MicroDustInner
-	{
-		 public const ushort G2G_MicroDust_LockRequest = 31002;
-		 public const ushort G2G_MicroDust_LockResponse = 31003;
-		 public const ushort G2M_MicroDust_SessionDisconnect = 31004;
-		 public const ushort M2M_MicroDust_UnitTransferRequest = 31005;
-		 public const ushort M2M_MicroDust_UnitTransferResponse = 31006;
-		 public const ushort R2G_MicroDust_LoginKey = 31007;
-		 public const ushort G2R_MicroDust_LoginKey = 31008;
-	}
+        [MemoryPackOrder(1)]
+        public int Error { get; set; }
+
+        [MemoryPackOrder(2)]
+        public string Message { get; set; }
+
+        [MemoryPackOrder(3)]
+        public long Key { get; set; }
+
+        [MemoryPackOrder(4)]
+        public long GateId { get; set; }
+
+        public override void Dispose()
+        {
+            if (!this.IsFromPool)
+            {
+                return;
+            }
+
+            this.RpcId = default;
+            this.Error = default;
+            this.Message = default;
+            this.Key = default;
+            this.GateId = default;
+
+            ObjectPool.Instance.Recycle(this);
+        }
+    }
+
+    public static class MicroDustInner
+    {
+        public const ushort G2G_MicroDust_LockRequest = 31002;
+        public const ushort G2G_MicroDust_LockResponse = 31003;
+        public const ushort G2M_MicroDust_SessionDisconnect = 31004;
+        public const ushort M2M_MicroDust_UnitTransferRequest = 31005;
+        public const ushort M2M_MicroDust_UnitTransferResponse = 31006;
+        public const ushort R2G_MicroDust_LoginKey = 31007;
+        public const ushort G2R_MicroDust_LoginKey = 31008;
+    }
 }
